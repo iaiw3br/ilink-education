809a44171ac608f44fb23de98858cf6b0867d606
Artem Ivanov <iaiw3br@gmail.com>
2020-09-08 21:12:16 +0300

    Подключает config для локальной базы postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {UserModule} from './user/user.module';
+import {typeOrmConfig} from "./config/typeorm.config";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+    imports: [
+        TypeOrmModule.forRoot(typeOrmConfig),
+        UserModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
