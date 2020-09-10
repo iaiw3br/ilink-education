581527e323e862e32317b25da0e6f0afa334b5d7
Artem Ivanov <iaiw3br@gmail.com>
2020-09-10 21:59:14 +0300

    Добавляет настройки для mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {GraphQLModule} from "@nestjs/graphql";
+import {UserModule} from './user/user.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "./user/user.entity";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mongodb',
+            url: 'mongodb://localhost/user',
+            synchronize: true,
+            useUnifiedTopology: true,
+            entities: [
+                User,
+            ]
+        }),
+        GraphQLModule.forRoot({
+            autoSchemaFile: true,
+        }),
+        UserModule
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
